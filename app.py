6743cd692caacaf572269190eb023904826e8294
fouadfouadbou99-boop <fouadfouadbou99@gmail.com>
2026-08-14 16:31:56 +0100

    Update and rename streamli_app.py to app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Modèle Immobilier OPCI",
+    page_icon="🏢",
+    layout="wide"
+)
+
+st.title("🏢 Modèle Immobilier OPCI")
+st.markdown("""
+Application d'évaluation financière immobilière :
+- Cash Flow
+- VAN
+- TRI
+- MOIC
+- Sensibilités
+- Dashboard Exécutif
+""")
